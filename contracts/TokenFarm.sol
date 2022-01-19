6c3b98954dac9b9a8d6539e418863ccf1ba1c671
Rad1m <radim.simanek@protonmail.ch>
2022-01-19 14:15:26 +0000

    half way through

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -11,13 +11,85 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenFarm is Ownable {
+    // mapping token address -> staker address -> amount
+    mapping(address => mapping(address => uint256)) public stakingBalance;
+    mapping(address => uint256) public uniqueTokensStaked;
+
+    address[] public stakers;
     address[] public allowedTokens;
+    IER20 public dappToken;
+
+    constructor(address _dappTokenAddress) public {
+        dappToken = IER20(_dappTokenAddress);
+    }
+
+    function issueTokens() public onlyOwner {
+        // Issue tokens to all stakers
+        for (
+            uint256 stakersIndex = 0;
+            stakersIndex < stakers.length;
+            stakersIndex++
+        ) {
+            address recipient = stakers[stakersIndex];
+            // send a token reward to stakers based on their TVL
+            uint256 userTVL = getUserTVL(recipient);
+            dappToken.transfer(recipient, TVL);
+        }
+    }
+
+    function getUserTVL(address _user) public view returns (uint256) {
+        uint256 totalValue = 0;
+        require(uniqueTokensStaked[_user] > 0, "No tokens staked!");
+        for (
+            uint256 allowedTokensIdex = 0;
+            allowedTokensIdex < allowedTokens.length;
+            allowedTokensIdex++
+        ) {
+            totalValue =
+                totalValue +
+                getUserSingleTokenValue(
+                    _user,
+                    allowedTokens[allowedTokensIndex]
+                );
+        }
+    }
+
+    function getUserSingleTokenValue(address _user, address _token)
+        public
+        view
+        returns (uint256)
+    {
+        if (uniqueTokensStaked[_user] <= 0) {
+            return 0;
+        }
+        // price of the token multipllied by staking balance
+        getTokenValue(_token);
+    }
+
+    function getTokenValue(address _token) public view returns (uint256) {
+        // chainlink price feed
+    }
 
     function stakeTokens(uint256 _amount, address _token) public {
         require(_amount > 0, "Amount must be more than 0");
-        // require token is allowed
+        require(tokenIsAllowed(_token), "Token is currently not allowed");
+        IER20(_token).transferFrom(msg.sender, address(this), _amount);
+        updateUniqueTokensStaked(msg.sender, _token);
+        stakingBalance[_token][msg.sender] =
+            stakingBalance[_token][msg.sender] +
+            _amount;
+        if (uniqueTokensStaked[msg.sender] == 1) {
+            stakers.push(msg.sender);
+        }
+    }
+
+    function updateUniqueTokensStaked(address user, address token) internal {
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
+        }
     }
 
     function addAllowedTokens(address _token) public onlyOwner {
