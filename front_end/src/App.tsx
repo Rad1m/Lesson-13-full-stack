eff4c079e2bbeb2d03d73493d657278f1b0dfb79
Rad1m <radim.simanek@protonmail.ch>
2022-01-22 16:23:25 +0000

    Connect button working

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ChainId, DAppProvider } from "@usedapp/core"
+import { Header } from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DAppProvider config={{
+      supportedChains: [ChainId.Kovan, ChainId.Rinkeby],
+    }}>
+      <Header />
+      <div>Hi!</div>
+    </DAppProvider >
+  )
 }
 
-export default App;
+export default App
