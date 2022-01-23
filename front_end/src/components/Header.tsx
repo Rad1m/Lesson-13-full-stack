71430970f07252cb6ea7fd8d64a774a2aa566da9
Rad1m <radim.simanek@protonmail.ch>
2022-01-23 12:20:18 +0000

    Material-ui implemented

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,26 +1,43 @@
 import { useEthers } from "@usedapp/core";
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: theme.spacing(4),
+        display: "flex",
+        justifyContent: "flex-end",
+        gap: theme.spacing(1)
+    },
+}))
 
 export const Header = () => {
+    const classes = useStyles()
+
     const { account, activateBrowserWallet, deactivate } = useEthers()
 
     const isConnected = account !== undefined
 
     return (
-        <div>
-            {isConnected ? (
-                <button color="primary" onClick={
-                    deactivate
-                }>
-                    Disconnect
-                </button>
-            ) : (
-                <button color="primary"
-                    onClick={() => activateBrowserWallet()}>
-                    Connect
+        <div className={classes.container}>
+            <div>
+                {isConnected ? (
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        onClick={
+                            deactivate
+                        }>
+                        Disconnect
+                    </Button>
+                ) : (
+                    <Button color="primary" variant="contained"
+                        onClick={() => activateBrowserWallet()}>
+                        Connect
 
-                </button>
-            )
-            }
+                    </Button>
+                )
+                }
+            </div>
         </div>
     )
 }
