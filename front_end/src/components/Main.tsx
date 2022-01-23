216a1a0944414c32962a05b73313f2213ef68131
Rad1m <radim.simanek@protonmail.ch>
2022-01-23 14:57:05 +0000

    Start of implementing wallet functionality

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -1,8 +1,21 @@
+/* eslink-disable spaced-comment */
+/// <reference types = "react-scripts" />
 import { useEthers } from "@usedapp/core"
 import helperConfig from "../helper-config.json"
 import networkMapping from "../chain-info/deployments/map.json"
 import { constants } from "ethers"
 import brownieConfig from "../brownie-config.json"
+import dapp from "../dapp.png"
+import eth from "../eth.png"
+import dai from "../dai.png"
+import { YourWallet } from "./yourWallet"
+import { makeStyles } from "@material-ui/core"
+
+export type Token = {
+    image: string
+    address: string
+    name: string
+}
 
 export const Main = () => {
     // Show token values from the wallet
@@ -20,9 +33,25 @@ export const Main = () => {
     const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero
     const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
 
+    const supportedTokens: Array<Token> = [
+        {
+            image: dapp,
+            address: dappTokenAddress,
+            name: "DAPP"
+        },
+        {
+            image: eth,
+            address: wethTokenAddress,
+            name: "WETH"
+        },
+        {
+            image: dai,
+            address: fauTokenAddress,
+            name: "DAI"
+        }
+    ]
 
-
-    return (<div>I'm message from Main.tsx</div>)
+    return (<YourWallet supportedTokens={supportedTokens} />)
 
 
 }
