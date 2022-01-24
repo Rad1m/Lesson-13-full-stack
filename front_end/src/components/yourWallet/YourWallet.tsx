fa7e470ed73ac01a193c51e706e63876801f748e
Rad1m <radim.simanek@protonmail.ch>
2022-01-24 12:58:07 +0000

    half way through

--- front_end/src/components/yourWallet/YourWallet.tsx
+++ front_end/src/components/yourWallet/YourWallet.tsx
@@ -1,9 +1,51 @@
 import { Token } from "../Main"
+import React, { useState } from "react"
+import { Box } from "@material-ui/core"
+import { TabContext, TabList, TabPanel } from "@material-ui/lab"
+import { Tab } from "@material-ui/core"
+import { WalletBalance } from "./WalletBalance"
+import { StakeForm } from "./StakeForm"
+
 
 interface YourWalletProps {
     supportedTokens: Array<Token>
 }
 
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
-    return (<div>I'm your wallet</div>)
+    const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0)
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setSelectedTokenIndex(parseInt(newValue))
+    }
+
+    return (
+        <Box>
+            <h1>Your wallet!</h1>
+            <Box>
+                <TabContext value={selectedTokenIndex.toString()}>
+                    <TabList onChange={handleChange} aria-label="stake from tabs">
+                        {supportedTokens.map((token, index) => {
+                            return (
+                                <Tab label={token.name}
+                                    value={indexedDB.toString()}
+                                    key={index} />
+                            )
+                        })}
+                    </TabList>
+                    {supportedTokens.map((token, index) => {
+                        return (
+                            <TabPanel value={index.toString()} key={index}>
+                                <div>
+                                    <WalletBalance token={supportedTokens[selectedTokenIndex]} />
+                                    <StakeForm token={supportedTokens[selectedTokenIndex]} />
+                                </div>
+                            </TabPanel>
+                        )
+                    })}
+                </TabContext>
+            </Box>
+        </Box>
+    )
+
+
 }
