03653db1f84b1b41763f9db37bcacd2b9ae2ebce
Rad1m <radim.simanek@protonmail.ch>
2022-02-12 17:38:38 +0000

    visual improvements

--- front_end/src/components/BalanceMsg.tsx
+++ front_end/src/components/BalanceMsg.tsx
@@ -1,34 +1,35 @@
-import { makeStyles } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
-    container: {
-        display: "inline-grid",
-        gridTemplateColumns: "auto auto auto",
-        gap: theme.spacing(1),
-        alignItems: "center"
-    },
-    tokenImg: {
-        width: "32px"
-    },
-    amount: {
-        fontWeight: 700
-    }
-}))
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "inline-grid",
+    gridTemplateColumns: "auto auto auto",
+    gap: theme.spacing(1),
+    alignItems: "center",
+  },
+  tokenImg: {
+    width: "32px",
+  },
+  amount: {
+    fontWeight: 600,
+    color: "red",
+  },
+}));
 
 interface BalanceMsgProps {
-    label: string
-    amount: number
-    tokenImgSrc: string
+  label: string;
+  amount: number;
+  tokenImgSrc: string;
 }
 
 export const BalanceMsg = ({ label, amount, tokenImgSrc }: BalanceMsgProps) => {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <div className={classes.container}>
-            <div>{label}</div>
-            <div className={classes.amount}>{amount}</div>
-            <img className={classes.tokenImg} src={tokenImgSrc} alt="token logo" />
-        </div>
-    )
-}
+  return (
+    <div className={classes.container}>
+      <div>{label}</div>
+      <div className={classes.amount}>{amount}</div>
+      <img className={classes.tokenImg} src={tokenImgSrc} alt="token logo" />
+    </div>
+  );
+};
---
+++ front_end/src/components/README.md
@@ -0,0 +1,2 @@
+Components folder is used to split code into several files (components)
+which makes it easier to edit and manage the code.
